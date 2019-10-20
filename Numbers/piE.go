56aeaa2c5b9b138dbb8f4317ab5331c85977f7ef
Parthvi Vala <pvala@redhat.com>
2019-10-20 15:35:41 +0530

    piE.go

---
+++ Numbers/piE.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+func main() {
+	var function, limit, printer string
+	fmt.Println("Enter a number: ")
+	fmt.Scan(&limit)
+	fmt.Println("Enter the function (pi/E): ")
+	fmt.Scan(&function)
+	formatter := "%." + limit + "f"
+	if function == "pi" {
+		printer = fmt.Sprintf(formatter, math.Pi)
+	} else if function == "E" {
+		printer = fmt.Sprintf(formatter, math.E)
+	} else {
+		printer = "You did not enter a valid function."
+	}
+	fmt.Println(printer)
+}
