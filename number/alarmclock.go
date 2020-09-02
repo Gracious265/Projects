ee302e749c140ab4102b28546ddc110742fd5ed2
Parthvi Vala <pvala@redhat.com>
2020-09-02 23:04:40 +0530

    Day 10: Made time.Sleep work. TODO: Play music

--- number/alarmclock.go
+++ number/alarmclock.go
@@ -3,21 +3,14 @@ package number
 import (
 	"fmt"
 	"time"
-	"math"
-	
-	// "io"
 )
 
 // AlarmClock : A simple clock where it plays a sound after X number of minutes/seconds or at a particular time.
 func AlarmClock() {
-	var minutes int64
-	var duration time.Duration
-	fmt.Print("Enter the number of minutes after which to ring the alarm: ")
-	fmt.Scan(&minutes)
-	minutes *= int64(math.Pow(10, 9))
-	// TODO: Fix this.
-	duration = time.Duration{minutes}
-	fmt.Printf("%v", minutes)
-	time.Sleep(duration)
+	var seconds int64
+	fmt.Print("Enter the number of seconds after which to ring the alarm: ")
+	fmt.Scan(&seconds)
+	time.Sleep(time.Duration(seconds)*time.Second)
 	fmt.Println("\nHey there!")
+// Check this : https://github.com/hajimehoshi/go-mp3/blob/master/example/main.go
 }
