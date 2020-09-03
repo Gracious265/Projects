8d4685bf30f56f1abd81b64b88840059f5394aaf
Parthvi Vala <pvala@redhat.com>
2020-09-03 23:43:26 +0530

    Day 11, finish building alarm clock

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.14
 require (
 	github.com/apaxa-go/eval v0.0.0-20171223182326-1d18b251d679
 	github.com/apaxa-go/helper v0.0.0-20180607175117-61d31b1c31c3 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.1
+	github.com/hajimehoshi/oto v0.6.4
 	golang.org/x/text v0.3.3 // indirect
 )
--- number/alarmclock.go
+++ number/alarmclock.go
@@ -1,9 +1,41 @@
 package number
 
 import (
+	"log"
 	"fmt"
 	"time"
+	"os"
+	"io"
+	"github.com/hajimehoshi/oto"
+	"github.com/hajimehoshi/go-mp3"
 )
+
+func playMusic() error {
+	f, err := os.Open("classic.mp3")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	d, err := mp3.NewDecoder(f)
+	if err != nil {
+		return err
+	}
+
+	c, err := oto.NewContext(d.SampleRate(), 2,2,8192)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	p := c.NewPlayer()
+	defer p.Close()
+
+	if _, err := io.Copy(p,d ); err != nil{
+		return err
+	}
+	return nil
+}
 
 // AlarmClock : A simple clock where it plays a sound after X number of minutes/seconds or at a particular time.
 func AlarmClock() {
@@ -11,6 +43,8 @@ func AlarmClock() {
 	fmt.Print("Enter the number of seconds after which to ring the alarm: ")
 	fmt.Scan(&seconds)
 	time.Sleep(time.Duration(seconds)*time.Second)
-	fmt.Println("\nHey there!")
+	if err := playMusic(); err != nil{
+		log.Fatal(err)
+	}
 // Check this : https://github.com/hajimehoshi/go-mp3/blob/master/example/main.go
 }
