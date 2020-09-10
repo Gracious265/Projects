dc78b0e6e77f578107c071b0aaa2d52bb110433e
Parthvi Vala <pvala@redhat.com>
2020-09-10 20:05:47 +0530

    Day 18, factorial finder

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	number.TaxCalculator()
+	number.FactorialFinder()
 }
---
+++ number/factorialfinder.go
@@ -0,0 +1,29 @@
+package number
+
+import (
+	"fmt"
+)
+
+func getFactorialRecursive(number int) int {
+	if number > 0 {
+		return number * getFactorialRecursive(number-1)
+	}
+	return 1
+}
+
+func getFactorialLooping(number int) int {
+	var factorial int = 1
+	for ;number >1; number-- {
+		factorial *= number
+	}
+	return factorial
+}
+
+// FactorialFinder : Finds factorial of a number.
+func FactorialFinder() {
+	var num int
+	fmt.Print("Enter number: ")
+	fmt.Scan(&num)
+	fmt.Println("Factorial via recusion is:", getFactorialRecursive(num))
+	fmt.Println("Factorial via looping is:", getFactorialLooping(num))
+}
